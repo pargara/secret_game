cc60a07ee83c8735b32392c0456b725db5ccd7d1
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-03-04 19:08:11 -0500

    Fixing the regex for the unsafe params validation

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,5 +1,8 @@
 class Area < ApplicationRecord
   has_many :employees
 
-  validates :name, presence: true, length: { minimum: 3 }, uniqueness: true, format: { with: /^[a-zA-Z0-9]*$/ }
+  validates :name, presence: { message: 'El nombre no puede estar vacio' }
+  validates :name, length: { minimum: 3, message: 'El nombre debe tener mas de 3 caracteres' }
+  validates :name, uniqueness: { message: 'El nombre del area no puede ser repetido' }
+  validates :name, format: { with: /\A[a-zA-Z0-9 ]*$\Z/, message: 'En el nombre solo pueden haber caracteres de a a-Z o numeros' }
 end
