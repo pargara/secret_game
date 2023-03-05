7456ef3c2b00a0dead0ca56ae32f60e0c56c5739
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-03-04 19:07:17 -0500

    Missing data key

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,10 +1,17 @@
 class AreasController < ApplicationController
   def index
     @areas = Area.all
+    render json: @areas, only: [:id, :name]
   end
 
-  def new
+  def create
     @area = Area.new(area_params)
+
+    if @area.save
+      render json: @area, only: [:id, :name], status: :created
+    else
+      render json: @area.errors, status: :unprocessable_entity
+    end
   end
 
   private
