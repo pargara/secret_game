8817e147f3cbe4e3c8b9d63beb048fa3189d1b7a
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-03-06 15:57:06 -0500

    Employees data done

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,15 +1,22 @@
 class EmployeesController < ApplicationController
   def index
-    @employees = Employee.all
+    @employees = Employee.includes(:area).all
+    render json: @employees, only: %i[id name], include: [area: { only: [:name] }]
   end
 
   def create
     @employee = Employee.new(employee_params)
+
+    if @employee.save!
+      render json: @employee, only: %i[id name], include: [area: { only: [:name] }], status: :created
+    else
+      render json: @employee, status: :unprocessable_entity
+    end
   end
 
   private
 
   def employee_params
-    params.require(:employee).permit(:name, :year_game)
+    params.require(:employee).permit(:name, :year_game, :area_id)
   end
 end
