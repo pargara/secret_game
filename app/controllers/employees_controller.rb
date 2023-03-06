3d12d32e1a24efb7e186f18a69727d3486dfedaf
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-03-06 16:54:16 -0500

    Validaciones basicas terminadas, acciones del controlador hechas

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,14 +1,14 @@
 class EmployeesController < ApplicationController
   def index
     @employees = Employee.includes(:area).all
-    render json: @employees, only: %i[id name], include: [area: { only: [:name] }]
+    render json: @employees, only: %i[id name year_game], include: [area: { only: [:name] }]
   end
 
   def create
     @employee = Employee.new(employee_params)
 
     if @employee.save!
-      render json: @employee, only: %i[id name], include: [area: { only: [:name] }], status: :created
+      render json: @employee, only: %i[id name year_game], include: [area: { only: [:name] }], status: :created
     else
       render json: @employee, status: :unprocessable_entity
     end
