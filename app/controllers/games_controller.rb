69bb7ecf44caf8c409c6416b053ce7b51defccd7
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-03-09 09:15:22 -0500

    Setting the couples, and left data

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,14 @@
 class GamesController < ApplicationController
   def index
     @games = Game.all
-    render json: @games, only: %i[id name]
+    render json: @games, only: %i[id couples left]
   end
 
   def create
     @game = Game.new(game_params)
+    @generator = PairGenerator.new
+    @game.couples = @generator.generate_pairs
+    @game.left = @generator.leftover_employee
 
     if @game.save!
       render json: @game, only: [:year], status: :created
@@ -17,6 +20,6 @@ class GamesController < ApplicationController
   private
 
   def game_params
-    params.require(:game).permit(:year, :couples, :left)
+    params.require(:game).permit(:year, :left, couples: [])
   end
 end
