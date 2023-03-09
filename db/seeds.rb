22b997e8306b908416cae0cc0f37a162b98171b1
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-03-09 09:14:39 -0500

    Adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+areas = Area.create([{ name: 'Tech' }, { name: 'HR' }])
+employees = Employee.create ([{ name: 'Sebastian Guevara', area_id: 1 },
+                              { name: 'Anderson Valencia', area_id: 1 },
+                              { name: 'Jose Gutierrez', area_id: 1 },
+                              { name: 'Robin Cardenas', area_id: 1 },
+                              { name: 'Juan David Duque Rendon', area_id: 2 }])
+game = Game.create(year: 2023)
