5341204f8a761504b8fdaf7a73c7e0c71a24a789
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-03-10 12:11:22 -0500

    solucion del problema al traer los datos

--- app/services/pair_generator.rb
+++ app/services/pair_generator.rb
@@ -2,20 +2,27 @@ class PairGenerator
   attr_accessor :leftover, :pairs
 
   def initialize
-    @employees = Employee.all
     @pairs = []
-    @leftover = nil
+    @names = []
+    employees_names
     generate_pairs
   end
 
   def generate_pairs
-    shuffle_employees = @employees.shuffle
+    shuffle_employees = @names.shuffle
     shuffle_employees.each_slice(2) do |a, b|
       if b.nil?
         @leftover = b
       else
         @pairs << [a, b]
       end
+    end
+  end
+
+  def employees_names
+    @employees = Employee.all
+    @employees.each do |position|
+      @names << position[:name]
     end
   end
 end
