bbbf233e45a778acfd100a4c0351b2ae8011fc38
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-03-13 10:42:38 -0500

    Adding a custom validation of minimum employees

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,11 @@ class Game < ApplicationRecord
   validates :year, presence: { message: 'El año de juego no puede ser vacio' }
   validates :year, inclusion: { in: (2023..2032), message: "%{value} no es un valor correcto, tiene
                                 que estar entre 2023 y 2032" }
+  validate :min_employees
+
+  private
+
+  def min_employees
+    errors.add(:base, 'Deben haber al menos 2 empleados') if Employee.count < 2
+  end
 end
