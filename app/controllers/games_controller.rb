b742a916aaef2a01ff047006842d84fe982f8efc
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-03-16 11:49:53 -0500

    El controlador valida si hay parejas repetidas pero hay que hacerlo con un until y el proposito del service esta jodido

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,7 +7,7 @@ class GamesController < ApplicationController
   def create
     @game = Game.new(game_params)
 
-    setting_names
+    create_couples
 
     if @game.save
       render json: @game, only: [:year], status: :created
@@ -22,9 +22,32 @@ class GamesController < ApplicationController
     params.require(:game).permit(:year, :left, :couples)
   end
 
-  def setting_names
+  def create_couples
     @generator = PairGenerator.new
     @game.couples = @generator.pairs
     @game.left = @generator.leftover
+    check_previous_game
+  end
+
+  def check_previous_game
+    previous_games = Game.where(year: (@game.year - 2)..(@game.year - 1)).order(year: :asc)
+    if previous_games.length >= 2
+      previous_game_pairs = previous_games.flat_map(&:couples)
+
+      previous_game_pairs_parsed = previous_game_pairs.map { |str| eval(str) }
+      games_couples_parsed = eval(@game.couples)
+
+      @has_repeated_pairs = contains_pairs(previous_game_pairs_parsed, games_couples_parsed)
+    end
+
+    create_couples if @has_repeated_pairs
+  end
+
+  def contains_pairs(array1, array2)
+    pairs = array1.flatten(1)
+    array2.each do |pair|
+      return true if pairs.include?(pair)
+    end
+    false
   end
 end
