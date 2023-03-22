cfe1bc83fbc98b4b3170e8b2ea7cdc2e50dedd0f
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-03-22 15:10:22 -0500

    Refactor done

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,10 +1,10 @@
 class Employee < ApplicationRecord
   belongs_to :area
   before_save :set_year
-  validates :area_id, presence: { message: 'El empleado tiene que estar asociado a un area' }
   validates :name, presence: { message: 'El empleado tiene que tener un nombre' }
   validates :name, length: { minimum: 3, message: 'El nombre debe tener almenos 3 caracteres' }
   validates :name, format: { with: /\A[a-zA-Z ]*$\Z/, message: 'En el nombre solo pueden haber caracteres de a a-Z' }
+  validates :area_id, presence: { message: 'El empleado tiene que estar asociado a un area' }
 
   private
 
