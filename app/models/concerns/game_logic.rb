671638a028e6af5f42fd296990dd108cdd7b8e33
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-03-22 12:36:02 -0500

    its working

--- app/models/concerns/game_logic.rb
+++ app/models/concerns/game_logic.rb
@@ -10,10 +10,11 @@ module GameLogic
 
   def all_employees(employees)
     @names = []
+
     @employees.each do |position|
       @names << position[:name]
     end
-    # binding.break
+
     generate_pairs(@names)
   end
 
@@ -28,13 +29,14 @@ module GameLogic
         @pairs << [a, b]
       end
     end
+
     self.couples = @pairs
     self.left = @leftover
   end
 
   def check_previous_game
     previous_games = Game.where(year: (self.year - 2)..(self.year - 1)).order(year: :asc)
-    if previous_games.length > 1
+    if previous_games.length >= 1
       previous_game_pairs = previous_games.flat_map(&:couples)
 
       previous_game_pairs_parsed = previous_game_pairs.map { |str| eval(str) }
@@ -46,17 +48,17 @@ module GameLogic
   end
 
   def create_couples_until_not_repeated(previous_game_pairs_parsed, games_couples_parsed)
-    @has_repeated_pairs = contains_pairs(previous_game_pairs_parsed, games_couples_parsed)
+    @has_repeated_pairs = contains_pairs?(previous_game_pairs_parsed, games_couples_parsed)
     # Jairo si ves la linea de abajo, sinceramente lo siento y se que no hay excusa para usar eso
     while @has_repeated_pairs
-      # create_couples
       setting_employees
+
       games_couples_parsed = eval(self.couples)
-      @has_repeated_pairs = contains_pairs(previous_game_pairs_parsed, games_couples_parsed)
+      @has_repeated_pairs = contains_pairs?(previous_game_pairs_parsed, games_couples_parsed)
     end
   end
 
-  def contains_pairs(previous_game_parsed, games_couples_parsed)
+  def contains_pairs?(previous_game_parsed, games_couples_parsed)
     pairs = previous_game_parsed.flatten(1)
 
     games_couples_parsed.each do |pair|
@@ -65,9 +67,23 @@ module GameLogic
     false
   end
 
+  def contains_the_leftover?
+    last_game_leftover = Game.where(year: (self.year - 1))
+    previous_game_leftover = last_game_leftover.flat_map(&:left)
+
+    return true if previous_game_leftover == self.left
+  end
+  
+  def change_leftover_until_not_repeated
+    while contains_the_leftover?
+      change_leftover
+      contains_the_leftover?
+    end
+  end
+
   def change_leftover
-    last_game = Game.where(year: (self.year - 1))
-    previous_game_leftover = last_game.flat_map(&:left)
+    last_game_leftover = Game.where(year: (self.year - 1))
+    previous_game_leftover = last_game_leftover.flat_map(&:left)
 
     if !previous_game_leftover.empty? && previous_game_leftover.include?(self.left)
       rand_position = rand(self.pairs.length)
